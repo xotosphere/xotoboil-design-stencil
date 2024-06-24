edc33348815a921d86e1019c0e683a552107987e
xotoverse <xotoverse@gmail.com>
2024-06-24 20:21:27 +0200

    fix : rename repo

--- stencil.config.ts
+++ stencil.config.ts
@@ -5,23 +5,23 @@ import { angularOutputTarget } from "@stencil/angular-output-target";
 import { reactOutputTarget } from "@stencil/react-output-target";
 
 export const config: Config = {
-  namespace: 'xotoboil-storybook-stencil',
+  namespace: 'xotoboil-design-stencil',
   outputTargets: [
     { type: "dist", esmLoaderPath: "../loader" },
     { type: "dist-custom-elements" },
     { type: "docs-readme" },
     { type: "www", serviceWorker: null },
     vueOutputTarget({
-      componentCorePackage: "xotoboil-storybook-stencil",
+      componentCorePackage: "xotoboil-design-stencil",
       proxiesFile: "./lib/vue/components.ts",
     }),
     angularOutputTarget({
-      componentCorePackage: "xotoboil-storybook-stencil",
+      componentCorePackage: "xotoboil-design-stencil",
       directivesProxyFile: "./lib/angular/components.ts",
       directivesArrayFile: "./lib/angular/directives.ts",
     }),
     reactOutputTarget({
-      componentCorePackage: "xotoboil-storybook-stencil",
+      componentCorePackage: "xotoboil-design-stencil",
       proxiesFile: "./lib/react/index.ts",
       includeDefineCustomElements: true,
     }),
